f440a7397874d493eacdb5f1b2d85e0a9d7b6613
khalajestani <khalajestani@gmail.com>
2021-05-27 14:34:28 +0200

    scan to query

--- lambda_functions/members_lambda/handler.py
+++ lambda_functions/members_lambda/handler.py
@@ -4,6 +4,7 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key, Attr
 
+
 def lambda_handler(event, context):
     member_table_name = os.environ['MEMBER_TABLE_NAME']
     members_table = boto3.resource('dynamodb').Table(member_table_name)
@@ -14,9 +15,10 @@ def lambda_handler(event, context):
                 'body': 'Id is mandatory!'
             }
         else:
-            member = members_table.scan(FilterExpression=Attr('id').eq(event['id']))
+            member = members_table.query(
+                KeyConditionExpression=Key('id').eq(event['id']))
             return member["Items"][0]
-            
+
     except Exception as general_exception:
         print(general_exception)
         print(traceback.format_exc())
--- lambda_functions/teams_lambda/handler.py
+++ lambda_functions/teams_lambda/handler.py
@@ -6,16 +6,18 @@ from boto3.dynamodb.conditions import Key, Attr
 
 import json
 
+
 def lambda_handler(event, context):
     teams_table_name = os.environ['TEAMS_TABLE_NAME']
     teams_table = boto3.resource('dynamodb').Table(teams_table_name)
     try:
         if event["action"] == "getTeamData":
-            if 'teamId' not in event or not event['teamId'] :
+            if 'teamId' not in event or not event['teamId']:
                 teams = teams_table.scan()
                 return teams["Items"]
             else:
-                team = teams_table.scan(FilterExpression=Attr('id').eq(event['teamId']))
+                team = teams_table.query(
+                    KeyConditionExpression=Key('id').eq(event['teamId']))
                 return team["Items"][0]
         elif event["action"] == "updateLastSprint":
             response = teams_table.update_item(
